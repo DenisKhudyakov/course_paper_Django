01bd82be55c7c40a3be6f03b89773296ad4a7a39
Denis Khudyakov <denis.hudyakov27@gmail.com>
2024-04-03 21:01:38 +0500

    add models

--- mailservice/models.py
+++ mailservice/models.py
@@ -1,2 +1,17 @@
 from django.db import models
 
+
+class Client(models.Model):
+    pass
+
+
+class MailingSettings(models.Model):
+    pass
+
+
+class Message(models.Model):
+    pass
+
+
+class MailingAttempts(models.Model):
+    pass
