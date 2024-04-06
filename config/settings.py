2b0ac63ec25f65dcb643c88dc4ef029246654fa7
Denis Khudyakov <denis.hudyakov27@gmail.com>
2024-04-06 11:13:31 +0500

    add models, get connect db, settings db

--- config/settings.py
+++ config/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,9 +80,9 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "mail_service",
-        "USER": "postgres",
-        "PASSWORD": "279211",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
     }
 }
 
--- mailservice/models.py
+++ mailservice/models.py
@@ -1,17 +1,80 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
+NULLABLE = {'blank': True, 'null': True}
 
 class Client(models.Model):
-    pass
 
+    """Клиент сервиса, поля email, ФИО, комментарий"""
 
-class MailingSettings(models.Model):
-    pass
+    email = models.EmailField(max_length=150, unique=True, verbose_name='почта')
+    name = models.CharField(max_length=100, verbose_name='ФИО')
+    comments = models.TextField(verbose_name='комментарий')
+
+    def __str__(self):
+        return f'{self.name}, {self.email}'
+
+    class Meta:
+        verbose_name = 'клиент'
+        verbose_name_plural = 'клиенты'
 
 
 class Message(models.Model):
-    pass
+
+    title = models.CharField(max_length=150, verbose_name='тема рассылки')
+    body = models.TextField(verbose_name='текст рассылки', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.title}, {self.body[:50]}...'
+
+    class Meta:
+        verbose_name = 'сообщение'
+        verbose_name_plural = 'сообщения'
 
 
-class MailingAttempts(models.Model):
-    pass
+class MailingSettings(models.Model):
+
+    """Класс рассылки, поля дата и время, периодичность(раз в день, раз в неделю, раз в месяц),
+    статус рассылки(завершена, создана, запущена)
+    """
+
+    class PeriodMailingSettings(models.TextChoices):
+        ONE_DAY = "Раз в день", _("Раз в день")
+        ONE_WEEK = "Раз в неделю", _("Раз в неделю")
+        ONE_MONTH = "Раз в месяц", _("Раз в месяц")
+
+    class StatusMailingSettings(models.TextChoices):
+        STARTED = "Запущена", _("Запущена")
+        CREATED = "Создана", _("Создана")
+        COMPLETED = "Завершена", _("Завершена")
+
+    date_and_time = models.DateField(verbose_name='дата и время первой рассылки')
+    period = models.CharField(max_length=50, choices=PeriodMailingSettings, verbose_name='периодичность рассылки')
+    status = models.CharField(max_length=50, choices=StatusMailingSettings, verbose_name='статус')
+    client = models.ManyToManyField(Client, verbose_name='клиенты рассылки')
+    message = models.ForeignKey(Message, verbose_name='сообщения', on_delete=models.CASCADE, related_name='message')
+
+    def __str__(self):
+        return f'{self.date_and_time},{self.period}, {self.status}, {self.client}'
+
+    class Meta:
+        verbose_name = 'настройка рассылки'
+        verbose_name_plural = 'настройки рассылки'
+
+
+class Logs(models.Model):
+    """Модель попытки рассылки, поля: дата и время последней рассылки, статус(успешно/не успешно)
+    ответ почтового сервера, если он был
+    """
+    date = models.DateField(verbose_name='дата и время последней рассылки', auto_now_add=True)
+    status = models.BooleanField(verbose_name='статус рассылки')
+    server_response = models.CharField(verbose_name='ответ почтового сервера', **NULLABLE)
+    message = models.ForeignKey(Message, verbose_name='сообщение', on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, verbose_name='клиент', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.date}, {self.status}'
+
+    class Meta:
+        verbose_name = 'лог'
+        verbose_name_plural = 'логи'
