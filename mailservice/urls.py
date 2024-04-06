2fdc3d14dc32a5ec7234d6f7d1086712aafdc691
Denis Khudyakov <denis.hudyakov27@gmail.com>
2024-04-06 14:37:01 +0500

    client finish

--- mailservice/urls.py
+++ mailservice/urls.py
@@ -1,11 +1,16 @@
 from mailservice.apps import MailserviceConfig
 from django.urls import path
-from mailservice.views import HomePageView, ClientListView
+from mailservice.views import HomePageView, ClientListView, ClientDetailView, ClientCreateView, ClientUpdateView, \
+    ClientDeleteView
 
 app_name = MailserviceConfig.name
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='homepage'),
     path('clients/', ClientListView.as_view(), name='clients'),
+    path('clients/<int:pk>/', ClientDetailView.as_view(), name='client-detail'),
+    path('create/', ClientCreateView.as_view(), name='create-client'),
+    path('<int:pk>/update/', ClientUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', ClientDeleteView.as_view(), name='delete-client')
 ]
 
--- mailservice/views.py
+++ mailservice/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.urls import reverse
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from mailservice.models import Client
 
 
@@ -10,5 +11,33 @@ class HomePageView(TemplateView):
 class ClientListView(ListView):
     """Список клиентов"""
     model = Client
+
+
+class ClientDetailView(DetailView):
+    """Просмотр одного клиента"""
+    model = Client
+
+
+class ClientCreateView(CreateView):
+    """Класс создания клиента"""
+    model = Client
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse('mailservice:clients')
+
+
+class ClientUpdateView(UpdateView):
+    """Класс редактирования клиента"""
+    model = Client
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse('mailservice:clients')
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    success_url = 'mailservice:clients'
+
 
 
